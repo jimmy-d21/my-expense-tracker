7e1b14de4540666436c23fab5547b558763562a1
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2025-10-05 01:39:36 +0800

    Declare all the doms

---
+++ script.js
@@ -0,0 +1,10 @@
+// Dom 
+const totalBalance = document.querySelector('.total-balance');
+const incomeAmount = document.querySelector('.income-amount');
+const expenseAmount = document.querySelector(".expense-amount");
+const transaxctionList = document.querySelector(".transaction-list");
+const form = document.querySelector(".transaction-form");
+const desacriptionInput = document.querySelector("#description");
+const amountInput = document.querySelector("#amount");
+
+
