db82dc8d78626911df12d6c80380dfe91a0f4a2d
jimmyedelacruz <jimmyedelacruz872@gmail.com>
2025-10-05 01:40:39 +0800

    Create a function to save to local storage and create a function for adding transaction

--- script.js
+++ script.js
@@ -8,3 +8,30 @@ const desacriptionInput = document.querySelector("#description");
 const amountInput = document.querySelector("#amount");
 
 
+const transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+
+// Update local storage
+function updateLocalStorage() {
+    localStorage.setItem("transactions", JSON.stringify(transactions));
+}
+
+form.addEventListener("submit", addTransaction);
+
+function addTransaction(e) {
+    e.preventDefault();
+    if(desacriptionInput.value.trim() === "" || amountInput.value.trim() === "") {
+        alert("Please enter a description and amount");
+        return;
+    }
+
+    const description = desacriptionInput.value;
+    const amount = parseFloat(amountInput.value);
+
+    const transaction = {
+        id: Date.now(),
+        description,
+        amount
+    }
+    transaction.push(transaction);
+    updateLocalStorage();
+}
